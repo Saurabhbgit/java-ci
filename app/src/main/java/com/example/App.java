06598bb311ba569d63ab8516dcf76bdd6f69dd8e
Your Name <saurabh.bhosale711@gmail.com>
2026-07-09 14:49:30 +0530

    add java project m

---
+++ app/src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static String greet() {
+        return "Hello GitHub Actions!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
